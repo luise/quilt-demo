66b076968a271691955014863f37a53e794543f3
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-11-12 16:39:58 -0800

    Initial spark.js code

---
+++ spark.js
@@ -0,0 +1,23 @@
+var defaultImage = "quilt/spark";
+
+function Spark(nWorker, image) {
+
+    var masterContainers = [new Container(image, ["run", "master"])]
+    this.masters = new Service("spark-ms", masterContainers)
+
+    var workerContainers = new Container(image, ["run", "worker"])
+        .withEnv({"MASTERS": this.masters.children().join(",")})
+        .replicate(nWorker);
+    this.workers = new Service("spark-wk", dkws);
+
+    this.workers.connect(7077, this.workers);
+    this.workers.connect(7077, this.masters);
+
+    this.deploy = function(deployment) {
+        deployment.deploy(this.masters);
+        deployment.deploy(this.workers);
+    }
+}
+
+exports.setImage = setImage;
+exports.Spark = Spark;
