96bc55c296675054dca502978dd9be93540c29cd
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-11-12 17:12:07 -0800

    tweak

--- spark.js
+++ spark.js
@@ -1,8 +1,8 @@
 var defaultImage = "quilt/spark";
 
 module.exports = function (nWorker, image) {
-    var masterContainers = [new Container(image, ["run", "master"])]
-    this.masters = new Service("spark-ms", masterContainers)
+    var masterContainers = [new Container(image, ["run", "master"])];
+    this.masters = new Service("spark-ms", masterContainers);
 
     var workerContainers = new Container(image, ["run", "worker"])
         .withEnv({"MASTERS": this.masters.children().join(",")})
