7a9e9fda508e70a893a708d6cc0a7683eeb9fd9b
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-11-12 20:12:37 -0800

    tweaks to run.js

--- run.js
+++ run.js
@@ -2,6 +2,7 @@ var Infrastructure = require("github.com/ejj/quilt-demo/infrastructure");
 var Spark = require("github.com/ejj/quilt-demo/spark");
 
 var sparkWorkers = 3;
-deployment = Infrastructure.create(sparkWorkers+ 1);
 spark = new Spark(sparkWorkers);
-deployment.deploy(spark);
+
+var inf = Infrastructure.create(sparkWorkers + 1);
+inf.deploy(spark);
