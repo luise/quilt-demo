bdc427e412a18e2b360223c979627c4b957e45cc
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-11-12 19:45:31 -0800

    Fewer workers

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 var Infrastructure = require("github.com/ejj/quilt-demo/infrastructure");
 var Spark = require("github.com/ejj/quilt-demo/spark");
 
-var sparkWorkers = 5;
+var sparkWorkers = 3;
 deployment = Infrastructure.create(sparkWorkers+ 1);
 spark = new Spark(sparkWorkers);
 deployment.deploy(spark);
