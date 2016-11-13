6717fcd9d5b08106dc23942159a3ed691cb1a6bf
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-11-13 12:25:08 -0800

    Hacking on the code

--- run.js
+++ run.js
@@ -2,5 +2,6 @@ var Inf = require("github.com/ejj/quilt-demo/inf");
 var Spark = require("github.com/ejj/quilt-demo/spark");
 
 var nSparkWorkers = 3;
+
 inf = Inf.New(nSparkWorkers + 1);
 Spark.New(inf, nSparkWorkers);
--- spark.js
+++ spark.js
@@ -1,16 +1,17 @@
 var defaultImage = "quilt/spark";
 
 exports.New = function (inf, sparkWorkers, image) {
-    if (typeof image !== 'string') {
-        image = defaultImage
-    }
-    var master = new Service("sprk-ms", [new Container(image, ["run", "master"])]);
-    var workers = new Service("sprk-wk", new Container(image, ["run", "worker"])
-                                             .withEnv({"MASTERS": master.hostname()})
-                                             .replicate(sparkWorkers));
-    workers.connect(7077, workers);
-    workers.connect(7077, master);
+  if (typeof image !== 'string') {
+      image = defaultImage
+  }
 
-    inf.deploy(master);
-    inf.deploy(workers);
+  var master = new Service("sprk-ms", [new Container(image, ["run", "master"])]);
+  var workers = new Service("sprk-wk", new Container(image, ["run", "worker"])
+                                           .withEnv({"MASTERS": master.hostname()})
+                                           .replicate(sparkWorkers));
+  workers.connect(7077, workers);
+  workers.connect(7077, master);
+
+  inf.deploy(master);
+  inf.deploy(workers);
 };
