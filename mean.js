0688ef3b957669e27a3d4d0e9d386ebce898159e
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2017-01-30 19:29:41 -0800

    Add a mean deployment

---
+++ mean.js
@@ -0,0 +1,23 @@
+var HaProxy = require("github.com/NetSys/quilt/specs/haproxy/haproxy").Haproxy;
+var Mongo = require("github.com/NetSys/quilt/specs/mongo/mongo");
+var Node = require("github.com/NetSys/quilt/specs/node/node");
+var Inf = Inf.New(4);
+
+var mongo = new Mongo(3);
+var app = new Node({
+  nWorker: 3,
+  image: "quilt/mean-service",
+  env: {
+    PORT: "80",
+    MONGO_URI: mongo.uri("mean-example")
+  }
+});
+var haproxy = new HaProxy(3, app.services());
+
+mongo.connect(mongo.port(), app);
+app.connect(mongo.port(), mongo);
+haproxy.public();
+
+Inf.deploy(app);
+Inf.deploy(mongo);
+Inf.deploy(haproxy);
