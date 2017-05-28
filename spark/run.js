a687f64c256e3d45171c3924313c543a1487aeaf
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2017-05-28 14:52:47 -0700

    Remove spark

--- spark/run.js
+++
@@ -1,7 +0,0 @@
-var Inf = require("github.com/ejj/quilt-demo/inf");
-var Spark = require("github.com/ejj/quilt-demo/spark");
-
-var nSparkWorkers = 3;
-
-inf = Inf.New(nSparkWorkers + 1);
-Spark.New(inf, nSparkWorkers);
--- spark/run2.js
+++
@@ -1,8 +0,0 @@
-var Inf = require("github.com/ejj/quilt-demo/inf");
-var Spark = require("github.com/ejj/quilt-demo/spark");
-
-var nSparkWorkers = 3;
-
-inf = Inf.New(nSparkWorkers + 1);
-prodSpark = Spark.New(inf, nSparkWorkers);
-devSpark = Spark.New(inf, nSparkWorkers);
